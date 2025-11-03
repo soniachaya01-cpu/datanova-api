f51c1b3a5a6f8d17339e7d3ad13c701cb57cc037
soniachaya01-cpu <sonia.chaya01@gmail.com>
2025-11-03 01:18:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.get("/")
@@ -6,4 +8,5 @@ def root():
     return {"status": "ok", "app": "datanova-api"}
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)
